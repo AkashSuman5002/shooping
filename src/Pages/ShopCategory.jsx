e72d78f0c557c5cd89bf1e6f311a1821da00056c
Akash Suman <168257687+AkashSuman5002@users.noreply.github.com>
2025-04-05 01:13:04 +0530

    Update ShopCategory.jsx

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -2,7 +2,7 @@ import React , {useContext } from 'react'
 import './CSS/ShopCategory.css'
 import { ShopContext } from '../Context/ShopContext';
 import dropdown_icon from '../Components/Asset/dropdown_icon.png'
-import Item from '../Components/Item/item';
+import Item from '../Components/Item/Item';
 
 
 const ShopCategory = (props) => {
